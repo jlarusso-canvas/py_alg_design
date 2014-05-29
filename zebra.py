a47b7d5dbd025044dbdedbd647247a6c71b5b42d
Jesse La Russo <jlarusso@canvas.is>
2014-05-29 17:00:18 -0400

    Zebra works

--- zebra.py
+++ zebra.py
@@ -1,3 +1,4 @@
+# http://en.wikipedia.org/wiki/Zebra_Puzzle
 import itertools
 
 
@@ -13,11 +14,23 @@ def zebra_puzzle():
   houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
   orderings = list(itertools.permutations(houses))
   return next((WATER, ZEBRA)
+
+    # Assign a color to each house
     for (red, green, ivory, yellow, blue) in c(orderings)
+
+    # Is house1 to the right of house2?
     if on_right(green, ivory)
+
+    # Assign each person to an ordering of houses
     for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in c(orderings)
+
+    # Is the englisman in the red house?
     if Englishman is red
+
+    # Is the norwegian in the first house?
     if Norwegian is first
+
+    # Etc.
     if next_to(Norwegian, blue)
     for (coffee, tea, milk, oj, WATER) in c(orderings)
     if coffee is green
@@ -34,7 +47,26 @@ def zebra_puzzle():
     if next_to(Kools, horse)
     )
 
+
 def c(sequence):
   return sequence
 
+
 print zebra_puzzle()
+
+
+
+def ints(start, end = None):
+  i = start
+  while i <= end or end is None:
+    yield i
+    i = i + 1
+
+
+def all_ints():
+  "Generate integers in the order 0, +1, -1, +2, -2, +3, -3, ..."
+  yield 0
+  for i in ints(1):
+    yield i
+    yield -i
+
