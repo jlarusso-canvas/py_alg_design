a03c1b06d3c802ba00c825e83f4fa169416162ec
Jesse La Russo <jlarusso@canvas.is>
2014-05-29 16:18:19 -0400

    Solve zebra

--- zebra.py
+++ zebra.py
@@ -1,18 +1,40 @@
 import itertools
 
 
-def imright(h1, h2):
+def on_right(h1, h2):
     return h1 - h2 == 1
 
 
-def nextto(h1, h2):
+def next_to(h1, h2):
     return abs(h1 - h2) == 1
 
 
 def zebra_puzzle():
   houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
   orderings = list(itertools.permutations(houses))
-  print orderings
+  return next((WATER, ZEBRA)
+    for (red, green, ivory, yellow, blue) in c(orderings)
+    if on_right(green, ivory)
+    for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in c(orderings)
+    if Englishman is red
+    if Norwegian is first
+    if next_to(Norwegian, blue)
+    for (coffee, tea, milk, oj, WATER) in c(orderings)
+    if coffee is green
+    if Ukranian is tea
+    if milk is middle
+    for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in c(orderings)
+    if Kools is yellow
+    if LuckyStrike is oj
+    if Japanese is Parliaments
+    for (dog, snails, fox, horse, ZEBRA) in c(orderings)
+    if Spaniard is dog
+    if OldGold is snails
+    if next_to(Chesterfields, fox)
+    if next_to(Kools, horse)
+    )
 
+def c(sequence):
+  return sequence
 
-zebra_puzzle()
+print zebra_puzzle()
